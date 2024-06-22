bfa09b2c6d0b62a113dea36c158920fdc53ecb03
nizomiddinuzb <bozorboyevnuriddin64@gmail.com>
2024-06-22 13:10:42 +0500

    second

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -15,7 +15,7 @@ async function SingleProduct({ params }: ParamsInterface) {
   const product = await request(params.id);
   console.log(product); 
   return (
-    <div className="items-center text-center w-[600px] h-[600px] mx-auto border-2 rounded-2xl">
+    <div className="items-center text-center w-[600px] h-[600px] mx-auto border-2 rounded-2xlg">
       <img className="ml-32" src={product.thumbnail} alt={product.title} />
       <h1 className="text-3xl mb-2">{product.title}</h1>
       <h2 className="text-2xl">{product.description}</h2>
